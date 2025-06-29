e0627348135c7a47684144200b2add8af0ce5e17
parmb410 <parmb410@gmail.com>
2025-06-28 21:27:21 -0400

    Update shap_utils.py

--- shap_utils.py
+++ shap_utils.py
@@ -6,21 +6,20 @@ class GNNWrapper(torch.nn.Module):
         super().__init__()
         self.model = model
     def forward(self, batch):
-        # batch is a single torch_geometric.data.Batch
         return self.model(batch)
 
 def explain_gnn_with_shap(model, data_loader, device, sample_count=10):
     model.eval()
-    # Get one *batched* graph (PyG Batch) from data_loader
     batch = next(iter(data_loader))
     if isinstance(batch, (list, tuple)):
-        batch_data = batch[0]  # (batch, labels) tuple
+        batch_data = batch[0]
     else:
         batch_data = batch
     batch_data = batch_data.to(device)
-    # For background, use a single Batch object (do NOT slice)
     background = batch_data
     wrapped_model = GNNWrapper(model)
-    explainer = shap.GradientExplainer(wrapped_model, background)
-    shap_values = explainer.shap_values(batch_data)
+    # ------ wrap background in a tuple ------
+    explainer = shap.GradientExplainer(wrapped_model, (background,))
+    # ------ wrap batch_data in a tuple ------
+    shap_values = explainer.shap_values((batch_data,))
     return shap_values, batch_data
